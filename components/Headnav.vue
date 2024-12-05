24e6298625992d7f3b141f42d82c311697c32dec
Marwin <contact@marwin-rodrigues.fr>
2024-12-05 19:24:05 +0100

    fix:HeaderRound_AddPres

--- components/Headnav.vue
+++ components/Headnav.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="sticky top-0">
     <div class="flex justify-center pt-10">
-    <div class="bg-secondary-color bg-opacity-20 w-1/2 rounded-2xl border-2 border-secondary-color border-opacity-50">
+    <div class="bg-secondary-color bg-opacity-20 w-7/12 rounded-full border-2 border-secondary-color border-opacity-50">
         <ul class="flex justify-center gap-10 py-2">
             <li>
                 <a href="#Home">
                     <p class="text-grey-color hover:text-white-color scale-100 hover:scale-105">Accueil</p>
+                </a>
+            </li>
+            <li>
+                <a href="#Pres">
+                    <p class="text-grey-color hover:text-white-color scale-100 hover:scale-105">Présentation</p>
                 </a>
             </li>
             <li>
