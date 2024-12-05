30f2bbc4cb43c2391353c86656882501a983bc61
Marwin <contact@marwin-rodrigues.fr>
2024-12-05 20:03:52 +0100

    feat:addNewComponent-CardBTS

---
+++ components/cardBTS.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+
+defineProps({
+    name: String,
+    img: String,
+    bio: String,
+})
+
+</script>
+
+<template>
+        <div class="container">
+  
+  <!-- Card 1 -->
+  <div class="card">
+
+      <div class="imgBx">
+
+          <img :src="`/imgs/${img}.jpg`" alt="Image1">
+
+      </div>
+
+      <div class="content">
+          <h2>{{ name }}</h2>
+          <p>{{ bio }}</p>
+      </div>
+
+  </div>   
+
+</div>    
+</template>
+
+<style>
+
+.container .card {
+    position: relative;
+    max-width: 300px;
+    height: 215px;
+    background: #FFFF;
+    /* margin: 30px 10px; */
+    /* padding: 20px 15px; */
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0 5px 20px rgba(0, 0, 0,0.5);
+    transition: 0.3s ease-in-out;
+}
+
+.container .card:hover {
+    height: 420px;
+}
+
+.container .card .imgBx {
+    position: relative;
+    width: 260px;
+    height: 260px;
+    top: -60px;
+    left: 20px;
+    z-index: 1;
+    box-shadow: 0 5px 20px rgba(0, 0, 0,0.2);
+}
+
+.container .card .imgBx img {
+    max-width: 100%;
+    border-radius: 4px;
+}
+
+.container .card .content {
+    position: relative;
+    margin-top: -140px;
+    padding: 10px 15px;
+    text-align: center;
+    color: #111;
+    visibility: hidden;
+    opacity: 0;
+    transition: 0.3s ease-in-out;
+}
+
+.container .card:hover .content {
+    visibility: visible;
+    opacity: 1;
+    margin-top: -40px;
+    transition-delay: 0.3s;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,40 @@
     <Headnav class="z-50"/>
 
     <!-- HERO -->
-    <section class="mt-20 pb-96" id="Home">
-        <h1 class="text-center text-grey-color font-Secondary font-bold text-6xl animate__animated animate__fadeInUp">Bienvenue sur mon Portfolio !</h1>
-        <div class="flex justify-center mt-10">
-            <p class="text-center text-secondary-color font-Primary w-1/2 text-lg animate__animated animate__slideInUp">Passionné par l'informatique et la cybersécurité, je me spécialise dans la gestion des infrastructures réseaux et la protection des systèmes. Mon objectif est de fournir des solutions fiables et sécurisées pour répondre aux enjeux technologiques des entreprises.</p>
+    <section class="mt-40 pb-96" id="Home">
+        <div class="relative">
+            <h1 class="text-center text-grey-color font-Secondary font-bold text-6xl animate__animated animate__fadeInUp">Bienvenue sur mon Portfolio !</h1>
+            <div class="flex justify-center mt-10">
+                <p class="text-center text-secondary-color font-Primary w-1/2 text-lg animate__animated animate__slideInUp">Passionné par l'informatique et la cybersécurité, je me spécialise dans la gestion des infrastructures réseaux et la protection des systèmes. Mon objectif est de fournir des solutions fiables et sécurisées pour répondre aux enjeux technologiques des entreprises.</p>
+            </div>
         </div>
     </section>
+
+    <section id="Pres" class="mb-[10%]">
+        <div class="flex justify-center gap-3">
+            <img src="/public/icons/stars-section.svg" alt="Icon Etoile - Titre section" class="w-[32px]">
+            <h1 class="text-center text-alert-color font-Secondary font-light text-3xl">Présentation du BTS SIO</h1>
+        </div>
+        <div class="flex justify-center mt-4">
+            <p class="text-secondary-color font-Primary w-1/2 text-lg">Actuellement étudiant en <b>BTS SIO (Services Informatiques aux Organisations)</b>, je me forme aux métiers de l’informatique, avec pour objectif de répondre aux besoins des entreprises en matière de systèmes d’information. Ce BTS propose une approche complète : nous apprenons à la fois la gestion des infrastructures, la sécurité, et le développement de solutions adaptées aux utilisateurs.</p>
+        </div>
+        <div class="grid grid-cols-2 mt-20 lg:ml-[20%]">
+            <CardBTS class="col-span-1" 
+            img="sisr"
+            name="SISR"
+            bio="SISR (Solutions d’Infrastructure, Systèmes et Réseaux) : axée sur l’administration et la gestion des réseaux et des systèmes. Cette option prépare à des missions comme la mise en place de réseaux sécurisés ou la maintenance des infrastructures informatiques."/>
+
+            <CardBTS class="col-span-1" 
+            img="SLAM"
+            name="SLAM"
+            bio="SLAM (Solutions Logicielles et Applications Métiers) : orientée vers le développement. Elle nous forme à concevoir des applications et logiciels sur mesure, en maîtrisant les langages de programmation et les bases de données."/>
+        </div>
+    </section>
+
+
+    <section>
+        <h2>test</h2>
+    </section>
+
 
 </template>
