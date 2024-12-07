8878f9eebcff11621daaa36dd0f18f1e149d3cb4
Marwin <contact@marwin-rodrigues.fr>
2024-12-07 15:30:25 +0100

    feat:SEO

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: 'Anthony Hecquet',
+  meta: [
+    { name: 'Anthony Hecquet', content: 'Anthony Hecquet' }
+  ],
+  })
+</script>
+
 <template>
   <div class="bg-primary-color">
     <NuxtLayout />
