aae7327789897c49e23082fcb36de5d612a9a416
Marwin <contact@marwin-rodrigues.fr>
2024-12-07 14:23:48 +0100

    fix:titleCardProject_CV

--- components/cardProjet.vue
+++ components/cardProjet.vue
@@ -12,7 +12,7 @@ defineProps({
 <template>
 
     <div class="grid gap-[3%]">
-        <h2 class="text-white-color font-Secondary font-light text-center">{{titre}}</h2>
+        <h2 class="text-white-color font-Secondary font-light text-center text-2xl">{{titre}}</h2>
         <div class="flex justify-center">
             <a :href="`/docs/projet/${link}.pdf`" target="_blank"> <img :src="`/docs/projet/${img}.jpg`" :alt="`Miniature du projet ${titre}`" class="w-[355px] rounded-xl scale-100 hover:scale-105 border-[0px] border-white-color hover:border-2"> </a>
         </div>
